53236eccc2b2e7ea452d5d51832e5385c03df2f1
Kevin Scaccia <kevin_developer@outlook.com>
2019-03-28 11:15:30 -0300

    Update KShell.c
    Multiple pipe operators testing

--- KShell.c
+++ KShell.c
@@ -22,6 +22,7 @@
 #define READ_END 0
 #define WRITE_END 1
 int GLOBAL_PIPE[2];
+int GLOBAL_PIPE_AUX[2];
 /*************************************** 
  * Protótipos
  ***************************************
@@ -30,10 +31,9 @@ int GLOBAL_PIPE[2];
 void loop_shell();
 void start_shell();
 void finish_shell();
-// Execução dos Comandos
-int execute_commands(char* line);
-// Manipulação dos Comandos
+// Manipulação e Execução dos Comandos
 char** split_commands(char* line);
+int execute_commands(char* line);
 char** get_command_parameters(char** matrix, int* index);
 // Funções e Procedimentos Auxiliares
 void print_commands(char** matrix);
@@ -42,8 +42,8 @@ int is_operador(char* c);
 int is_operador_char(char c);
 int is_command(char* c);
 void print_error(char* c);
-void print_aviso(char* c);
-
+void print_alert(char* c);
+void printf_alert(char* format, char* c);
 /*************************************** 
  * Main
  ***************************************
@@ -101,7 +101,7 @@ char** split_commands(char* line){
  ***************************************
  ***************************************/
 void execute_pipe_operand(char* commands){
-	print_aviso("Pipe Operator detected!");
+	print_alert("Pipe Operator detected!");
 	// comandos separados por um pipe
 	char* command_0 = commands;
 	int i = 0;
@@ -115,7 +115,7 @@ char** get_command_parameters(char** matrix, int* index){
 	int i = *index;  // le argumentos a partir do comando atual
 	// inclui o nome do comando como argumento
 	int ii = 0;
-	printf("Comando na posicao <%d> da matriz de comandos\n",i);
+	//printf("Comando na posicao <%d> da matriz de comandos\n",i);
 	char* arg = matrix[i];  // argumento atual
 	while(arg != NULL && !is_operador(arg)){
 		//printf("Adicionando <%s> como argumento \n", arg);
@@ -127,100 +127,171 @@ char** get_command_parameters(char** matrix, int* index){
 	*index = i;
 	return args;
 }
+int operador_antes(char** commands, int* index){
+	pid_t filho;
+	if(TRUE){//commands[*index-1][0] == '|'){
+		// Como existe operador antes, o pipe ja foi criado
+		char** com_mat = get_command_parameters(commands, index);// ANTES DO FORK
+		filho = fork();
+		if(filho == 0){
+			close(GLOBAL_PIPE[WRITE_END]);// fecha pois FILHO nao escreve no pipe
+			// redireciona o READ END do pipe para a entrada do filho
+			dup2(GLOBAL_PIPE[READ_END], STDIN_FILENO); //duplica leitura do pipe sobre entrada padrao
+			if(execvp(com_mat[0], com_mat) < 0){
+				print_error("Comando possívelmente inválido ou incompleto:");
+				perror("->");
+				//return SHELL_STATUS_CONTINUE;
+			}
+		}else{
+			close(GLOBAL_PIPE[WRITE_END]); // pipe terminado
+			close(GLOBAL_PIPE[READ_END]); // pipe terminado
+			waitpid(filho, NULL, 0);
+			return SHELL_STATUS_CONTINUE;
+		}		
+		return SHELL_STATUS_CONTINUE;
+	}else{
+		return SHELL_STATUS_CONTINUE;
+	}
+}
+int operador_depois(char** commands, int* index){
+	pid_t filho;
+	// resolver operador
+	if(TRUE){//commands[*index+1][0] == '|'){
+		print_alert("pipe encontrado");
+		char** com_mat = get_command_parameters(commands, index);
+		// Cria pipe pro acesso
+		if(pipe(GLOBAL_PIPE) < 0){
+			print_error("Erro ao criar PIPE");
+		}else{
+			filho = fork();
+			if(filho == 0){  // Processo Filho
+				close(GLOBAL_PIPE[READ_END]);// fecha pois FILHO nao lê no pipe
+				// duplica saida padrao do filho para escrita do pipe
+				dup2(GLOBAL_PIPE[WRITE_END], STDOUT_FILENO); 
+				// substitui espaço de endereçamento (comando atual[i])
+				if(execvp(com_mat[0], com_mat) < 0){
+					print_error("Comando possívelmente inválido ou incompleto:");
+					perror("->");
+					return SHELL_STATUS_CONTINUE;
+				}
+			}else{
+				// Espera pelo filho antes de ler o 
+				// proximo comando
+				waitpid(filho, NULL, 0);
+			}
+			// Redireciona saida do commando atual
+			// para o pipe, para posterior uso
+		}
+		return SHELL_STATUS_CONTINUE;
+	}
+	return SHELL_STATUS_CONTINUE;
+}
+int sem_operador(char** commands, int* index){
+	pid_t filho;
+	char** com_mat = get_command_parameters(commands, index);
+	//print_commands(com_mat);
+	filho = fork();
+	if(filho == 0){
+		if(execvp(com_mat[0], com_mat) < 0){
+				print_error("Comando possívelmente inválido ou incompleto:");
+				perror("->");
+				return SHELL_STATUS_CONTINUE;
+			}else{
+				printf("Executado corretamente");
+			}
+	}else{
+		waitpid(filho, NULL, 0);
+	}
+	return SHELL_STATUS_CONTINUE;
+}
+int operador_antes_e_depois(char** commands, int* index){
+	pid_t filho;
+	if(TRUE){//commands[*index-1][0] == '|'){
+		// Como existe operador antes, o pipe ja foi criado
+		char** com_mat = get_command_parameters(commands, index);// ANTES DO FORK
+
+		if(pipe(GLOBAL_PIPE_AUX) < 0){
+			print_error("Erro ao criar PIPE");
+		}
+
+
+
+		filho = fork();
+		if(filho == 0){
+			// LE DO PIPE ANTIGO
+			dup2(GLOBAL_PIPE[READ_END], STDIN_FILENO); //duplica leitura do pipe sobre entrada padrao
+			// redireciona o READ END do pipe para a entrada do filho
+			// duplica saida padrao do filho para escrita do pipe
+			// ESCREVE NO PIPE NOVO
+			char* inbuf = (char*)malloc(sizeof(char)*1000);
+        		while(read(GLOBAL_PIPE[READ_END], inbuf, 1000))
+        			printf("CONTEUDO: %s \n", inbuf); 
+  
+
+			dup2(GLOBAL_PIPE_AUX[WRITE_END], STDOUT_FILENO); 
+			if(execvp(com_mat[0], com_mat) < 0){
+				print_error("Comando possívelmente inválido ou incompleto:");
+				perror("->");
+				//return SHELL_STATUS_CONTINUE;
+			}
+		}else{
+			// ATUALIZA PARA O PIPE NOVO
+			//printf(">>>>>>>>>>>%d", GLOBAL_PIPE_AUX[READ_END]);
+			GLOBAL_PIPE[READ_END] = GLOBAL_PIPE_AUX[READ_END];
+			GLOBAL_PIPE[WRITE_END] = GLOBAL_PIPE_AUX[WRITE_END];
+			waitpid(filho, NULL, 0);
+			return SHELL_STATUS_CONTINUE;
+		}		
+		return SHELL_STATUS_CONTINUE;
+	}else{
+		return SHELL_STATUS_CONTINUE;
+	}
+}
+int ha_operador_antes(char** commands, int index){
+	if(index <= 0)
+		return FALSE;
+	char* cmd = commands[index];
+	while(cmd != NULL && index >= 0){
+		if(is_operador(cmd))
+			return TRUE;// ha operador depois
+		cmd = commands[--index];
+	}
+	return FALSE;
+}
+int ha_operador_depois(char** commands, int index){
+	char* cmd = commands[index];
+	while(cmd != NULL){
+		if(is_operador(cmd))
+			return TRUE;// ha operador depois
+		cmd = commands[++index];
+	}
+	return FALSE;
+}
 int execute_commands(char* line){
 	char** commands = split_commands(line);
-	print_commands(commands);
-	pid_t filho;
-	// pesquisa por operadores
+	//print_commands(commands);
 	int i = 0;
 	while(commands[i] != NULL){
 		if(is_command(commands[i])){  // encontrou um COMANDO
-			// verifica se há OPERADOR ANTES do comando
-			if( (i > 0) && is_operador(commands[i-1])){
-				print_aviso("Ha operador antes");
-				// Operaador e o pipe
-				if(commands[i-1][0] == '|'){
-					// Como existe operador antes, o pipe ja foi criado
-					char** com_mat = get_command_parameters(commands, &i);// ANTES DO FORK
-					printf("%s",com_mat[0]);
-					filho = fork();
-					if(filho == 0){
-						// redireciona o READ END do pipe para a entrada do filho
-						dup2(GLOBAL_PIPE[READ_END], STDIN_FILENO); //duplica leitura do pipe sobre entrada padrao
-						
-						if(execvp(com_mat[0], com_mat) < 0){
-							print_error("Comando possívelmente inválido ou incompleto:");
-							perror("->");
-							return SHELL_STATUS_CONTINUE;
-						}else{
-							printf("Executado corretamente");
-							return SHELL_STATUS_CONTINUE;
-						}
-					}else{
-						waitpid(filho, NULL, 0);
-						return SHELL_STATUS_CONTINUE;
-					}		
-					return SHELL_STATUS_CONTINUE;
-				}
-			}else if(is_operador(commands[i+1])){
-				// verifica se há OPERADOR APÓS o comando
-				print_aviso("Ha operador depois");
-				// resolver operador
-				if(commands[i+1][0] == '|'){
-					print_aviso("pipe encontrado");
-					char** com_mat = get_command_parameters(commands, &i);
-					// Cria pipe pro acesso
-					if(pipe(GLOBAL_PIPE) < 0){
-						print_error("Erro ao criar PIPE");
-					}else{
-						filho = fork();
-						if(filho == 0){  // Processo Filho
-							// duplica saida padrao do filho para escrita do pipe
-							dup2(GLOBAL_PIPE[WRITE_END], STDOUT_FILENO); 
-							// substitui espaço de endereçamento (comando atual[i])
-							if(execvp(com_mat[0], com_mat) < 0){
-								print_error("Comando possívelmente inválido ou incompleto:");
-								perror("->");
-								return SHELL_STATUS_CONTINUE;
-							}else{
-								printf("Executado corretamente");
-								return SHELL_STATUS_CONTINUE;
-							}
-						}else{
-							// Espera pelo filho antes de ler o 
-							// proximo comando
-							waitpid(filho, NULL, 0);
-						}
-						// Redireciona saida do commando atual
-						// para o pipe, para posterior uso
-					}
-				}else if(commands[i+1][0] == '&'){
-					continue;
-				}
-				//print_aviso("Há operador");
-			}else{  // não há operador
-				print_aviso("Nao ha operador");
-				char** com_mat = get_command_parameters(commands, &i);
-				//printf("Passado para executar: <%s> com os comandos:\n", com_mat[0]);
-				//print_commands(com_mat);
-				filho = fork();
-				if(filho == 0){
-					if(execvp(com_mat[0], com_mat) < 0){
-							print_error("Comando possívelmente inválido ou incompleto:");
-							perror("->");
-							return SHELL_STATUS_CONTINUE;
-						}else{
-							printf("Executado corretamente");
-						}
+			if(ha_operador_antes(commands, i)){
+				if(ha_operador_depois(commands, i)){
+					print_alert("Ha operador antes e depois");
+					operador_antes_e_depois(commands, &i);
 				}else{
-					waitpid(filho, NULL, 0);
+					print_alert("Ha operador antes");
+					operador_antes(commands, &i);
 				}
+			}else if(ha_operador_depois(commands, i)){
+				print_alert("Ha operador depois");
+				operador_depois(commands, &i);
+			}else{
+				print_alert("Nao ha operador");
+				sem_operador(commands, &i);
 			}
 		}
 		i++;
 	}
 	printf("\033[0m");
-
 	return SHELL_STATUS_CONTINUE;
 }
 /*************************************** 
@@ -243,7 +314,7 @@ void read_line(char* buffer){
 }
 void print_commands(char** matrix){
 	int i =0;
-	printf("\033[1;35m");
+	printf("\033[1;35m---------------\n Comandos:\n");
 	while(matrix[i] != NULL){
 		if(is_operador(matrix[i]))
 			printf("	Operador %d: (%s)\n", i, matrix[i]);
@@ -253,7 +324,7 @@ void print_commands(char** matrix){
 			print_error("	Operador/Comando invalido");*/
 		i++;
 	}
-	printf("\033[0m");
+	printf("---------------\033[0m\n");
 }
 int is_operador_char(char c){
 	if( c  == '|' || c == '&' || c == '<' || c == '>')
@@ -288,6 +359,11 @@ int is_command(char* c){
 void print_error(char* c){
 	printf("\033[1;31mErro: %s\033[0m\n", c);
 }
-void print_aviso(char* c){
-	printf("\033[01;33m\"\"%s\"\"\033[0m\n", c);
+void print_alert(char* c){
+	printf("\033[01;33m--> %s \033[0m\n", c);
+}
+void printf_alert(char* format, char* c){
+	printf("\033[01;33m--> ");
+	printf(format, c);
+	printf("\033[0m\n");
 }
